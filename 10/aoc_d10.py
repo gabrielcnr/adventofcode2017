f6f2e5007ef9743f321fb73d7f9ee7fb74241749
Gabriel Reis <gabrielcnr@gmail.com>
2017-12-10 11:07:19 +0000

    Day 10 - part 1 - completed

--- 10/aoc_d10.py
+++ 10/aoc_d10.py
@@ -1,5 +1,6 @@
 # numbers from 0..255
-numbers = range(256)
+numbers = list(range(256))
+print(len(numbers))
 
 # current position
 pos = 0
@@ -11,19 +12,37 @@ lengths = [83, 0, 193, 1, 254, 237, 187, 40, 88, 27, 2, 255, 149, 29, 42, 100]
 
 
 # test data
-numbers = [0, 1, 2, 3, 4]
-lengths = [3, 4, 1, 5]
+# numbers = [0, 1, 2, 3, 4]
+# lengths = [3, 4, 1, 5]
 
 for length in lengths:
-    numbers_subset = numbers[pos:pos+length]
+    if length > 1:
+        end_pos = pos + length
+        if end_pos > len(numbers):
+            second_end_pos = end_pos % len(numbers)
+            numbers_subset = numbers[pos:] + numbers[:second_end_pos]
+        else:
+            numbers_subset = numbers[pos:end_pos]
 
-    reversed_numbers = list(reversed(numbers_subset))
+        reversed_numbers = list(reversed(numbers_subset))
 
-    numbers = reversed_numbers + numbers[pos+length:]
+        if end_pos > len(numbers):
+            second_end_pos = end_pos % len(numbers)
+
+            numbers = reversed_numbers[-second_end_pos:] + numbers[second_end_pos:pos]  + reversed_numbers[:-second_end_pos]
+        else:
+            numbers[pos:end_pos] = reversed_numbers
 
     pos = (pos + length + skip) % len(numbers)
 
     skip += 1
 
-    print(numbers)
+    # print(numbers)
+    # print('pos ', pos)
+    # print('skip', skip)
+    #
+    # print('\n')
 
+print(numbers)
+print(numbers[0] * numbers[1])
+print(len(numbers))
